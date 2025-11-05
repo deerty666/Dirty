35ec1e4c6a2621d4a94de3fbc75dd874a5b8265e
deerty666 <alraiee318@gmail.com>
2025-11-05 03:37:18 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'deerty-menu-cache-v1';
+const urlsToCache = [
+    '/',
+    'index.html',
+    'style.css',
+    'script.js',
+    'manifest.json',
+    'images/logo-192.png',
+    'images/logo-512.png'
+];
